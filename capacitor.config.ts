f93650d2fa5c64c1987caf862b29e2ae98f75fad
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2025-03-16 03:28:23 +0600

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'basic.grocery.list',
+  appName: 'basic-grocery-list',
+  webDir: 'www'
+};
+
+export default config;
