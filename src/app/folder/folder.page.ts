ea648d9fcedaea2815c118c6c9aacfac38d88bf7
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2025-03-16 03:55:26 +0600

    add tailwind and rearranging

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,12 +1,26 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
-  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonMenuButton,
+    IonTitle,
+    IonContent,
+  ],
 })
 export class FolderPage implements OnInit {
   public folder!: string;
@@ -17,3 +31,52 @@ export class FolderPage implements OnInit {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
 }
+
+// import { addIcons } from 'ionicons';
+// import {
+//   mailOutline,
+//   mailSharp,
+//   paperPlaneOutline,
+//   paperPlaneSharp,
+//   heartOutline,
+//   heartSharp,
+//   archiveOutline,
+//   archiveSharp,
+//   trashOutline,
+//   trashSharp,
+//   warningOutline,
+//   warningSharp,
+//   bookmarkOutline,
+//   bookmarkSharp,
+// } from 'ionicons/icons';
+
+// public appPages = [
+//   { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
+//   { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
+//   { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
+//   { title: 'Archived', url: '/folder/archived', icon: 'archive' },
+//   { title: 'Trash', url: '/folder/trash', icon: 'trash' },
+//   { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+// ];
+// public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+// constructor() {
+//   // addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
+// }
+
+// imports: [
+//   RouterLink,
+//   RouterLinkActive,
+//   IonApp,
+//   IonSplitPane,
+//   IonMenu,
+//   IonContent,
+//   IonList,
+//   IonListHeader,
+//   IonNote,
+//   IonMenuToggle,
+//   IonItem,
+//   IonIcon,
+//   IonLabel,
+//   IonRouterLink,
+//   IonRouterOutlet,
+// ],
